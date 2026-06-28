520ad3a9a2c9f1a8444499c2a1ae9083de5b943b
Ifechukwu <ezekwemifee@gmail.com>
2026-06-28 14:23:28 +0100

    fixed db issue

--- attendance_app_backend/settings.py
+++ attendance_app_backend/settings.py
@@ -100,14 +100,14 @@ CHANNEL_LAYERS = {
 
 if os.environ.get("DATABASE_URL"):
     DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), 
+                                      conn_max_age=600)
 }
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
-
         }
     }
 
